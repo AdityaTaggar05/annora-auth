a7d35d0bfadd6915cac81de883484d028e064456
AdityaTaggar05 <aditya05.mgg@gmail.com>
2025-12-28 20:02:49 +0530

    Added basic middlewares

--- internal/handler/https/router.go
+++ internal/handler/https/router.go
@@ -4,10 +4,14 @@ import (
 	authhandler "github.com/AdityaTaggar05/annora-auth/internal/handler/https/auth"
 	tokenhandler "github.com/AdityaTaggar05/annora-auth/internal/handler/https/token"
 	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 )
 
 func NewRouter(authHandler *authhandler.Handler, tokenHandler *tokenhandler.Handler) *chi.Mux {
 	r := chi.NewRouter()
+
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
 
 	r.Post("/auth/register", authHandler.HandleRegister)
 	r.Post("/auth/login", authHandler.HandleLogin)
